a573f8a9d8ac8f6986d529e0997094a50064bb05
David Renz <davidrenz60@gmail.com>
2016-01-07 16:59:10 -0800

    add calculator.rb

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,28 @@
+#ask user for two numbers
+#ask user for operator
+#perform operation
+#output result
+
+Kernel.puts("Welcome to Calculator!")
+
+Kernel.puts("What is your first number?")
+number1 = Kernel.gets().chomp()
+
+Kernel.puts("What is your second number?")
+number2 = Kernel.gets().chomp()
+
+Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+  result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
+elsif operator == '4'
+  result = number1.to_f() / number2.to_f()  
+end
+
+Kernel.puts("The result is #{result}")
+
