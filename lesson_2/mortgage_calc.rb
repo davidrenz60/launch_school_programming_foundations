c129f7910b1fd451d6d26ffdc1972eb97346e1dc
David Renz <davidrenz60@gmail.com>
2016-01-18 20:20:17 -0800

    move code from main block to methods

--- lesson_2/mortgage_calc.rb
+++ lesson_2/mortgage_calc.rb
@@ -14,56 +14,59 @@ def valid_num?(input)
   (integer?(input) || float?(input)) && input.to_f >= 0
 end
 
+def loan_entry
+  loop do
+    amount = gets.chomp
+    break amount if valid_num?(amount)
+    prompt("Please enter a valid loan amount")
+  end
+end
+
+def interest_entry
+  loop do
+    interest = gets.chomp
+    break interest if valid_num?(interest)
+    prompt("Please enter a valid percentage")
+  end
+end
+
+def years_entry
+  loop do
+  years = gets.chomp
+    break years if integer?(years) && years.to_i >= 0
+    prompt("Please enter a vaild number of years")
+  end
+end
+
+def calculate_payment(amount, apr, years)
+  monthly_interest = apr.to_f / 100 / 12
+  months = years.to_i * 12
+  amount.to_f * (monthly_interest / (1 - ((1 + monthly_interest)**-months)))
+end
+
+def start_over?
+  prompt("Would you like to calculate another loan? Enter 'Y' to continue")
+  answer = gets.chomp
+  answer.downcase.start_with?('y')
+end
+
+
 prompt("Welcome to the Mortgage Calculator!")
 
 loop do
-  prompt("What is the loan amount?")
-
-  amt = ''
-  loop do
-    amt = gets.chomp
-    if valid_num?(amt)
-      break
-    else
-      prompt("Please enter a valid loan amount")
-    end
-  end
-
+  prompt("What is the amount of the loan?")
+  amount = loan_entry
+  
   prompt("What is the Annual Percentage Rate? Example: enter 2.8 for 2.8%")
-
-  apr = ''
-  loop do
-    apr = gets.chomp
-    if valid_num?(apr)
-      break
-    else
-      prompt("Please enter a valid percentage")
-    end
-  end
-
+  apr = interest_entry
+  
   prompt("What is the duration of the loan? Enter the amount of years")
+  years = years_entry
 
-  years = ''
-  loop do
-    years = gets.chomp
-    if integer?(years) && years.to_i >= 0
-      break
-    else
-      prompt("Please enter a vaild number of years")
-    end
-  end
+  monthly_payment = calculate_payment(amount, apr, years)
 
-  monthly_interest = apr.to_f / 100 / 12
-  months = years.to_i * 12
-  amt = amt.to_f
-
-  pmt = amt * (monthly_interest / (1 - ((1 + monthly_interest)**-months)))
-
-  prompt("The monthly payment for this loan is $#{format('%2.2f', pmt)}")
-
-  prompt("Would you like to calculate another loan? Enter 'Y' to continue")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  prompt("The monthly payment for this loan is $#{format('%2.2f', monthly_payment)}")
+  break unless start_over?
 end
 
 prompt("Thanks for using the Mortgage Calculator!")
